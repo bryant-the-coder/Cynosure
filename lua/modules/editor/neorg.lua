e8fde258bd0a6ef0e96099e9e44828488df4b032
bryant-the-coder <bryantfoo99@gmail.com>
2022-05-26 18:53:49 +0800

    fix(norg): Added export for neorg

--- lua/modules/editor/neorg.lua
+++ lua/modules/editor/neorg.lua
@@ -24,6 +24,7 @@ neorg.setup({
                 type = "auto",
             },
         },
+        ["core.export"] = {},
         ["core.export.markdown"] = {
             config = {
                 extensions = "all",
