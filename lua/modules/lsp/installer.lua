4fb07431bde2258f85dc188111ded2183a6081eb
bryant-the-coder <bryantfoo99@gmail.com>
2022-05-29 14:23:40 +0800

    feat(installer): Added icons and max_concurrent_installers

--- lua/modules/lsp/installer.lua
+++ lua/modules/lsp/installer.lua
@@ -14,4 +14,14 @@ local servers = {
 -- Setting up installer
 require("nvim-lsp-installer").setup({
     ensure_installed = servers,
+    ui = {
+        icons = {
+            server_installed = " ",
+            server_pending = " ",
+            server_uninstalled = " ﮊ",
+        },
+    },
+    -- Limit for the maximum amount of servers to be installed at the same time. Once this limit is reached, any further
+    -- servers that are requested to be installed will be put in a queue.
+    max_concurrent_installers = 3,
 })
