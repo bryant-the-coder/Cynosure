042512eb018304ce623a8dde23b3f8110f87de27
bryant-the-coder <bryantfoo99@gmail.com>
2022-06-01 14:24:23 +0800

    feat(tools): Added border for Colortils

--- lua/modules/tools/colortils.lua
+++ lua/modules/tools/colortils.lua
@@ -3,7 +3,41 @@ if not present then
     return
 end
 
+-- local border = {
+--     { "┏", "FloatBorder" },
+--     { "━", "FloatBorder" },
+--     { "┓", "FloatBorder" },
+--     { "┃", "FloatBorder" },
+--     { "┛", "FloatBorder" },
+--     { "━", "FloatBorder" },
+--     { "┗", "FloatBorder" },
+--     { "┃", "FloatBorder" },
+-- }
+
+-- local border = {
+-- 	{ "╔", "FloatBorder" },
+-- 	{ "═", "FloatBorder" },
+-- 	{ "╗", "FloatBorder" },
+-- 	{ "║", "FloatBorder" },
+-- 	{ "╝", "FloatBorder" },
+-- 	{ "═", "FloatBorder" },
+-- 	{ "╚", "FloatBorder" },
+-- 	{ "║", "FloatBorder" },
+-- }
+
+local border = {
+    { "╭", "FloatBorder" },
+    { "─", "FloatBorder" },
+    { "╮", "FloatBorder" },
+    { "│", "FloatBorder" },
+    { "╯", "FloatBorder" },
+    { "─", "FloatBorder" },
+    { "╰", "FloatBorder" },
+    { "│", "FloatBorder" },
+}
+
 color.setup({
     register = "+", -- register in which color codes will be copied: any register
-    color_display = "block", -- how to display the color: "block" or "hex"
+    color_preview = "█ %s", -- preview for colors, if it contains `%s` this will be replaced with a hex color code of the color
+    border = border,
 })
