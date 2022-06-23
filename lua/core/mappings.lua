e6d2ad1bb648fd353a54e4acbc9cab913520615e
bryant-the-coder <bryantfoo99@gmail.com>
2022-06-23 22:32:34 +0800

    feat(mappings): Added lsp mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -196,6 +196,7 @@ end)
 map("n", "<C-j>", function()
     vim.diagnostic.goto_next { border = "rounded" }
 end)
+map("n", "<leader>lbh", vim.lsp.buf.hover, { desc = "Show the documentation in a floating window" })
 
 -- Harpooon
 map("n", "<A-p>", function()
