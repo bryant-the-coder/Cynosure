6610cb6f213c14bb24ebb19def4f5e5a42e72d69
bryant-the-coder <bryantfoo99@gmail.com>
2022-06-28 22:51:59 +0800

    feat(snippets): Added a new lua snippet call status

--- luasnippets/lua.lua
+++ luasnippets/lua.lua
@@ -11,7 +11,15 @@ local mappings = [[
 map("$1", "$2", "<cmd>$0<CR>")
 ]]
 
+local status = [[
+local status_ok, ${1:module_var_name} = pcall(require, "${2:module_name}")
+if not status_ok then
+    return
+end
+]]
+
 return {
     parse({ trig = "M" }, module),
     parse({ trig = "map" }, mappings),
+    parse({ trig = "status" }, status),
 }
