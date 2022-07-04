56e2d6f34d02609a7b907056116d8b58b0337616
bryant-the-coder <bryantfoo99@gmail.com>
2022-07-04 20:18:47 +0800

    moved some lsp stuff to other files

--- lua/modules/lsp/config.lua
+++ lua/modules/lsp/config.lua
@@ -113,5 +113,8 @@ local config = {
 
 vim.diagnostic.config(config)
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border, width = 60 })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+    vim.lsp.handlers.signature_help,
+    { border = border, witdth = 60 }
+)
