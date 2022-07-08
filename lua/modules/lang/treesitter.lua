7d42765ade0e0c07c1459e2d10b7de29d4f1c9a8
bryant-the-coder <bryantfoo99@gmail.com>
2022-07-08 21:15:49 +0800

    testing to see if the ts error proceeds

--- lua/modules/lang/treesitter.lua
+++ lua/modules/lang/treesitter.lua
@@ -69,9 +69,9 @@ treesitter.setup {
         enable = true,
         enable_autocmd = false,
     },
-    query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = { "BufWrite", "CursorHold" },
-    },
+    -- query_linter = {
+    --    enable = true,
+    --    use_virtual_text = true,
+    --    lint_events = { "BufWrite", "CursorHold" },
+    -- },
 }
