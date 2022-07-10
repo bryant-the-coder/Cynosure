07796fc94bb8a3fd4655f5f095123a432c8d7888
bryant-the-coder <bryantfoo99@gmail.com>
2022-07-10 12:56:55 +0800

    formatter

--- lua/modules/lang/formatter.lua
+++ lua/modules/lang/formatter.lua
@@ -1,29 +1,67 @@
--- Utilities for creating configurations
-local util = require "formatter.util"
+local function clang_format()
+    return {
+        exe = "clang-format",
+        args = { "--assume-filename", vim.api.nvim_buf_get_name(0) },
+        stdin = true,
+        cwd = vim.fn.expand "%:p:h",
+    }
+end
 
-require("formatter").setup {
-    -- All formatter configurations are opt-in
+local function stylua()
+    local util = require "formatter.util"
+    return {
+        exe = "stylua",
+        args = {
+            "--search-parent-directories",
+            "--stdin-filepath",
+            util.escape_path(util.get_current_buffer_file_path()),
+            "--",
+            "-",
+        },
+        stdin = true,
+    }
+end
+
+local function yapf()
+    return { exe = "yapf", args = { vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)) }, stdin = true }
+end
+
+local function isort()
+    return { exe = "isort", args = { "-" }, stdin = false }
+end
+
+local function gofmt()
+    return { exe = "gofmt", args = { "-w" }, stdin = false }
+end
+
+local function prettier()
+    return {
+        exe = "prettier",
+        args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)) },
+        stdin = true,
+    }
+end
+
+local function rustfmt()
+    return { exe = "rustfmt", args = { "--emit=stdout" }, stdin = true }
+end
+
+local function google_java_format()
+    return { exe = "google-java-format", args = { vim.api.nvim_buf_get_name(0) }, stdin = true }
+end
+
+local formatter = require "formatter"
+formatter.setup {
     filetype = {
-        cpp = {
-            require("formatter.filetypes.cpp").clangformat,
-        },
-        lua = {
-            require("formatter.filetypes.lua").stylua,
-        },
-        rust = {
-            require("formatter.filetypes.rust").rustfmt,
-        },
-        html = {
-            require("formatter.filetypes.html").prettier,
-        },
-        css = {
-            require("formatter.filetypes.css").prettier,
-        },
-        javascript = {
-            require("formatter.filetypes.javascript").prettier,
-        },
-        python = {
-            require("formatter.filetypes.python").black,
-        },
+        c = { clang_format },
+        cpp = { clang_format },
+        css = { prettier },
+        go = { gofmt },
+        html = { prettier },
+        java = { google_java_format },
+        javascript = { prettier },
+        lua = { stylua },
+        python = { yapf, isort },
+        rust = { rustfmt },
     },
 }
