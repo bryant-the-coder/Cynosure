90c13b5a7dcf0dde242c09f879417ab50bb36d84
bryant-the-coder <bryantfoo99@gmail.com>
2022-07-31 18:37:53 +0800

    feat(autocmd): Add new autocmd call AutoRun

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,6 +1,7 @@
+local api = vim.api
 local cmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
--- local create_command = vim.api.nvim_create_user_command
+local create_command = vim.api.nvim_create_user_command
 
 -- Disable autocommenting {{{
 cmd("BufEnter", {
@@ -168,4 +169,34 @@ cmd({ "VimEnter", "DirChanged" }, {
         end
     end,
 })
+-- }}}
+
+-- Autorun function {{{
+local attach_to_buffer = function(output_bufnr, pattern, command)
+    api.nvim_create_autocmd("BufWritePost", {
+        group = api.nvim_create_augroup("autorun", { clear = true }),
+        pattern = pattern,
+        callback = function()
+            local append_data = function(_, data)
+                if data then
+                    api.nvim_buf_set_lines(output_bufnr, -1, -1, false, data)
+                end
+            end
+            api.nvim_buf_set_lines(output_bufnr, -1, -1, false, { "Output of file: " })
+            vim.fn.jobstart(command, {
+                stdout_buffered = true,
+                on_stdout = append_data(),
+                on_stderr = append_data(),
+            })
+        end,
+    })
+end
+
+create_command("AutoRun", function()
+    print "Autorun will start..."
+    local bufnr = vim.fn.input "Bufnr: "
+    local pattern = vim.fn.input "Pattern: "
+    local command = vim.split(vim.fn.input "Command: ", " ")
+    attach_to_buffer(tonumber(bufnr), pattern, command)
+end, {})
 -- }}}
