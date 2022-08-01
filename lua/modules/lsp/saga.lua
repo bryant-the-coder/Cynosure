07b3748de86fa67076cd1988429715eb708efcd7
bryant-the-coder <bryantfoo99@gmail.com>
2022-08-02 00:22:33 +0800

    add lsp saga mappings

--- lua/modules/lsp/saga.lua
+++ lua/modules/lsp/saga.lua
@@ -92,6 +92,7 @@ map("n", "<C-k>", require("lspsaga.diagnostic").goto_prev) ]]
 map("n", "<leader>ld", require("lspsaga.definition").preview_definition)
 -- show hover doc and press twice will jumpto hover window
 map("n", "<leader>lh", require("lspsaga.signaturehelp").signature_help)
+vim.keymap.set("n", "<leader>lf", require("lspsaga.finder").lsp_finder)
 
 -- scroll down hover doc or scroll in definition preview
 map("n", "<C-f>", function()
