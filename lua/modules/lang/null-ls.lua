dd5d6d79d66ad034251cce668d23c87b2dda55ea
bryant-the-coder <bryantfoo99@gmail.com>
2022-09-07 15:26:22 +0800

    lol null ls settings changed

--- lua/modules/lang/null-ls.lua
+++ lua/modules/lang/null-ls.lua
@@ -2,7 +2,7 @@ local null_ls = require "null-ls"
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
--- local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local utils = require "core.utils"
 
@@ -13,6 +13,7 @@ null_ls.setup {
         formatting.stylua,
         formatting.rustfmt,
         formatting.clang_format,
+        -- diagnostics.flake8,
         -- formatting.yapf,
     },
 
